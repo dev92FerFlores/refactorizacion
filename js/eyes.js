24e9745ec365acdd043bf261def9bab097e282aa
dev92ferflores <joytech2000@gmail.com>
2024-09-17 12:15:03 -0600

    refactorizacion de la pagina a nivel codigo de esta manera ya se realizo un scope es decir se introdujo el codigo en un try-catch para evitar errores y en caso de etenerlos saber cual es el error tambien se actualiza la sintaxis del codigo

--- js/eyes.js
+++ js/eyes.js
@@ -1,15 +1,20 @@
-var balls = document.getElementsByClassName("ball");
-document.onmousemove = function(){
-    var x = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
-    
-    
-    for(var i=0;i<2;i++){
-        balls[i].style.left=x;
-        balls[i].style.top=y;
-        balls[i].style.transform = "traslate(-"+x+",-"+y+")";
+try{
+    const balls = document.querySelectorAll(".ball");
 
-
-    }
+    console.log(balls);
     
-        }
+    document.addEventListener("mousemove", (event) => {
+        let x = (event.clientX * 100)/ window.innerWidth + "%";
+        let y = (event.clientY * 100) / window.innerHeight + "%";
+        
+        balls.forEach((ball) => {
+            ball.style.left = x;
+            ball.style.top = y;
+            ball.style.transform = "traslate(-" + x + ",-" + y + ")";
+        });
+    
+    });
+}
+catch (err){
+    console.log(err);
+}
